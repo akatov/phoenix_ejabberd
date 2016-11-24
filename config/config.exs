29dca41f11057406f34b79d862892a6d44e3c203
Dmitri Akatov <akatov@gmail.com>
2016-11-24 21:53:55 +0000

    app configuration for ejabberd

--- config/config.exs
+++ config/config.exs
@@ -18,6 +18,13 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+config :ejabberd,
+  file: "config/ejabberd.yml",
+  log_path: "logs/ejabberd.log"
+
+config :mnesia,
+  dir: "mnesiadb/"
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
